f2a97bd2fcd18acbdc9800db010699308c9056a6
rjauquet <rjauquet@gmail.com>
2015-09-21 19:16:18 -0500

    adding first functions

--- dev/main.js
+++ dev/main.js
@@ -1,8 +1,30 @@
-var photational = function (){
-	var self 		= {},
-		photational = {};
+var photational = function (args){
 	
-	return photational;	
+	
+		
+	
+	if(!args || args.id === undefined){
+		return undefined;
+	} else {
+		var self 		= {},				
+			photational = {};
+
+		self.id = args.id;
+
+				
+
+		photational.getId = function (){
+			return self.id;
+		};
+		photational.setId = function (id){
+			if(id !== undefined){
+				self.id = id;
+			}
+			return photational;
+		};
+
+		return photational;
+	}		
 }
 
 module.exports = photational;
